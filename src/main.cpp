3f56d88c041e5cc726a4cca0c466b681a82e1beb
Tomasz Scislowicz <toomasz@gmail.com>
2020-05-27 20:00:40 +0200

    relay support

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,13 @@
 ThingifyEsp thing("Shelly1");
 
 
+const int RelayPin = 4;
+const int SwitchPin = 5;
+
 bool OnBoolChanged(void*_, Node *node)
 {
-  bool value = node->Value.AsBool();
-  if(value)
-  {
-    Serial.printf("Bool node changed to true");
-  }
-  else
-  {
-    Serial.printf("Bool node changed to false");
-  }
-  
-
-	return true;
-}
-
-bool OnNodeValueChanged(void*_, Node *node)
-{
-  Serial.printf("'%s' node changed value to '%s'\n", node->name(), node->Value.toString().c_str());
-	return true;
+    digitalWrite(RelayPin, node->Value.AsBool());
+  	return true;
 }
 
 void setup()
@@ -31,10 +18,9 @@ void setup()
 	Serial.begin(500000);	
 	thing.AddDiagnostics();
 
-  thing.AddBoolean("bool")->OnChanged(OnNodeValueChanged);
-  thing.AddInt("integer", ThingifyUnit::Volt)->SetValue(NodeValue::Int(220));
-  thing.AddRange("range", 10, 30, 2, ThingifyUnit::Percent)->OnChanged(OnNodeValueChanged);
-  thing.AddColor("Color")->OnChanged(OnNodeValueChanged);
+  pinMode(RelayPin, OUTPUT);
+
+  thing.AddBoolean("Switch")->OnChanged(OnBoolChanged);
 	thing.Start();
 }
 
